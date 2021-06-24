6b47d3e417481bba4e5e9f713c709025c21e4562
Tristan Bietsch <tristanmbietsch@gmail.com>
2021-06-24 15:20:59 -0500

    task one and two done

--- classification-challenge.py
+++ classification-challenge.py
@@ -12,3 +12,17 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import numpy
 
+DIRECTORY = 'Covid19-dataset/train'
+CLASS_MODE = 'catagorical'
+COLOR_MODE = 'greyscale'
+TARGET_SIZE = (256, 256)
+BATCH_SIZE  = 32
+
+training_data_generator = ImageDataGenerator(
+    rescale=1.0/255, 
+    zoom_range=0.1, 
+    rotation_range= 25,
+    width_shift_range=0.05, 
+    height_shift_range=0.05
+)
+
